c759ab9a571fca20a53e6d26e54915a712f8a5f3
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-11-22 11:34:07 -0500

    Updates

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import styles from "./styles.module.scss"
+import data from "./data/data.json";
+import styles from "./styles.module.scss";
 import NavBar from "./navbar/NavBar";
-import Event from "./Event";
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <NavBar />
-      <Event />
+      {data.map((items) => {
+        return (
+          // PARENT CONTAINER EVENT NAME
+          <div>
+            <div>
+              <h1>{items.name}</h1>
+            </div>
+
+            {items.markets.map((items) => {
+              return (
+                <>
+                {/* CHILD CONTAINER MARKET NAME */}
+                  <div>
+                    <h2>{items.name}</h2>
+
+                    {items.selections.map((items) => {
+                      return (
+                        <>
+                        {/* GRANDCHILD CONTAINER SELECTION COMPONENT */}
+                          <div>
+                            <button>
+                              {items.name} {items.price}
+                            </button>
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
